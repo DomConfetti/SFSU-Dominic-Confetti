490dccf935a19925aa8fe2cb6d49b2a1ef5181c1
Dominic Confetti <domsvllc@gmail.com>
2020-09-01 14:17:12 -0700

    Initial Commit

---
+++ SFSU-Dominic-Confetti/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SFSU-Dominic-Confetti
+//
+//  Created by Dominic Confetti on 9/1/20.
+//  Copyright © 2020 SFSU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SFSU-Dominic-ConfettiTests/SFSU_Dominic_ConfettiTests.swift
@@ -0,0 +1,34 @@
+//
+//  SFSU_Dominic_ConfettiTests.swift
+//  SFSU-Dominic-ConfettiTests
+//
+//  Created by Dominic Confetti on 9/1/20.
+//  Copyright © 2020 SFSU. All rights reserved.
+//
+
+import XCTest
+@testable import SFSU_Dominic_Confetti
+
+class SFSU_Dominic_ConfettiTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
