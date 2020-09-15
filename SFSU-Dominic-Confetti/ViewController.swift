d0f58ec90de42c007fec83ea03910dcb4e056e25
Dominic Confetti <dconfetti@mail.sfsu.edu>
2020-09-15 10:34:46 -0700

    updated for HW 3

--- SFSU-Dominic-Confetti/ViewController.swift
+++ SFSU-Dominic-Confetti/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        messageLabel.text = ""
+        
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        
+        messageLabel.text = " Anonymous !"
+        imageView.image = UIImage(named: "423529")
+    }
+    
 
 }
 
